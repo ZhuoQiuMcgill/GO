c40829cec8202f3ea5e928b0fb45c8116d594b69
Uncle-Ma11 <1933511939@qq.com>
2023-05-29 17:36:17 -0400

    Finish move check alg to test

--- Board.py
+++ Board.py
@@ -5,9 +5,9 @@ from collections import deque
 
 class Board:
 
-    #1 黑棋
-    #-1 白棋
-    #0 无子
+    # 1 黑棋
+    # -1 白棋
+    # 0 无子
     def __init__(self, size):
         self.size = size
         self.board = np.zeros((size, size), dtype=int)
@@ -17,29 +17,78 @@ class Board:
         #     for j in range (size):
         #         self.stone_sets.append()
 
-        #打劫
+        # 打劫
+        self.Ko = False
         self.last_Ko_x = None
         self.last_Ko_y = None
 
     def black_move(self, x, y):
-        if x == self.last_Ko_x and y == self.last_Ko_y:
+        if self.Ko and x == self.last_Ko_x and y == self.last_Ko_y:
             return "Ko error"
         elif self.board[x][y] != 0:
             return "Stone error"
         else:
             self.board[x][y] = 1
-            self.check_capture(self, 1, x, y)
+            self.move_check(1, x, y)
             self.next_player = -1
 
     def white_move(self, x, y):
-        if x == self.last_Ko_x and y == self.last_Ko_y:
+        if self.Ko and x == self.last_Ko_x and y == self.last_Ko_y:
             return "Ko error"
         elif self.board[x][y] != 0:
             return "Stone error"
         else:
             self.board[x][y] = -1
-            self.check_capture(self, -1, x, y)
+            self.move_check(-1, x, y)
             self.next_player = 1
+
+    def move_check(self, cur_player, x, y):
+        captured = self.check_capture(self, cur_player, x, y)
+        # 如果没有吃子，检测是否不入气
+        if captured == 0:
+            self.Ko = False
+            if self.check_suicide(1, x, y) > 0:
+                return "Suicide error"
+        # 如果只吃一子，检测打劫
+        elif captured == 1:
+            count = 0
+            if self.board[x - 1, y] if x > 0 else None == -1:
+                count += 1
+            else:
+                pos = 1
+            if self.board[x + 1, y] if x < self.size - 1 else None == -1:
+                count += 1
+            else:
+                pos = 2
+            if self.board[x, y - 1] if y > 0 else None == -1:
+                count += 1
+            else:
+                pos = 3
+            if self.board[x, y + 1] if y < self.size - 1 else None == -1:
+                count += 1
+            else:
+                pos = 4
+            if count == 3:
+                self.Ko = True
+                if pos == 1:
+                    self.last_Ko_x = x - 1
+                    self.last_Ko_y = y
+                elif pos == 2:
+                    self.last_Ko_x = x + 1
+                    self.last_Ko_y = y
+                elif pos == 3:
+                    self.last_Ko_x = x
+                    self.last_Ko_y = y - 1
+                elif pos == 4:
+                    self.last_Ko_x = x
+                    self.last_Ko_y = y + 1
+                else:
+                    print("bug")
+            else:
+                self.Ko = False
+        else:
+            self.Ko = False
+
 
     # def move_union(self, current_player, x, y):
     #     top = self.board[x - 1, y] if x > 0 else None
@@ -51,59 +100,66 @@ class Board:
     #     else:
     #         if top == current_player:
 
-
-
-
+    # 一步棋吃子的数量
     def check_capture(self, current_player, x, y):
         capt_num = 0
         top = self.board[x - 1, y] if x > 0 else None
         bottom = self.board[x + 1, y] if x < self.size - 1 else None
         left = self.board[x, y - 1] if y > 0 else None
         right = self.board[x, y + 1] if y < self.size - 1 else None
-        if current_player == 1:
-            if top == -1:
-                self.check_suicide(self, current_player, x - 1, y)
-            if bottom == -1:
-                self.check_suicide(self, current_player, x + 1, y)
-            if left == -1:
-                self.check_suicide(self, current_player, x, y - 1)
-            if right == -1:
-                self.check_suicide(self, current_player, x, y + 1)
-        elif current_player == -1:
-            if top == 1:
-                self.check_suicide(self, current_player, x - 1, y)
-            if bottom == 1:
-                self.check_suicide(self, current_player, x + 1, y)
-            if left == 1:
-                self.check_suicide(self, current_player, x, y - 1)
-            if right == 1:
-                self.check_suicide(self, current_player, x, y + 1)
-        else:
-            return
+        if top == -current_player:
+            capt_num += self.check_suicide(self, -current_player, x - 1, y)
+        if bottom == -current_player:
+            capt_num += self.check_suicide(self, -current_player, x + 1, y)
+        if left == -current_player:
+            capt_num += self.check_suicide(self, -current_player, x, y - 1)
+        if right == -current_player:
+            capt_num += self.check_suicide(self, -current_player, x, y + 1)
+        # elif current_player == -1:
+        #     if top == 1:
+        #         capt_num += self.check_suicide(self, current_player, x - 1, y)
+        #     if bottom == 1:
+        #         capt_num += self.check_suicide(self, current_player, x + 1, y)
+        #     if left == 1:
+        #         capt_num += self.check_suicide(self, current_player, x, y - 1)
+        #     if right == 1:
+        #         capt_num += self.check_suicide(self, current_player, x, y + 1)
+        return capt_num
 
+    # 检测一块棋是否还有气， BFS
     def check_suicide(self, current_player, x, y):
         visited = set()
         queue = deque([x, y])
 
         while queue:
             node = queue.popleft()
-            x_val = node[0]
-            y_val = node[1]
-            top = self.board[x_val - 1, y_val] if x > 0 else None
-            bottom = self.board[x_val + 1, y_val] if x < self.size - 1 else None
-            left = self.board[x_val, y_val - 1] if y > 0 else None
-            right = self.board[x_val, y_val + 1] if y < self.size - 1 else None
             if node not in visited:
+                x_val = node[0]
+                y_val = node[1]
+                top = self.board[x_val - 1, y_val] if x_val > 0 else None
+                bottom = self.board[x_val + 1, y_val] if x_val < self.size - 1 else None
+                left = self.board[x_val, y_val - 1] if y_val > 0 else None
+                right = self.board[x_val, y_val + 1] if y_val < self.size - 1 else None
+                visited.add(node)
 
+                if top == 0 or bottom == 0 or left == 0 or right == 0:
+                    return 0
+                else:
+                    if top == current_player and (x_val - 1, y_val) not in visited:
+                        queue.append((x_val - 1, y_val))
+                    if bottom == current_player and (x_val + 1, y_val) not in visited:
+                        queue.append((x_val + 1, y_val))
+                    if left == current_player and (x_val, y_val - 1) not in visited:
+                        queue.append((x_val, y_val - 1))
+                    if right == current_player and (x_val, y_val + 1) not in visited:
+                        queue.append((x_val, y_val + 1))
 
-
-
-
+        for x_, y_ in visited:
+            self.board[x_][y_] = 0
+        return len(visited)
 
     def stop(self, next_player):
         if self.next_player == 1:
             self.next_player = -1
         elif self.next_player == -1:
             self.next_player = 1
-
-

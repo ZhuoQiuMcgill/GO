c698532c28154fea5ebc695ac0eca7ac978e76c2
ZhuoQiuMcgill <QIUZHUOWORK@gmail.com>
2023-05-30 16:05:13 -0400

    add step counter

--- gui.py
+++ gui.py
@@ -20,6 +20,7 @@ CURRENT_BOARD = []
 GAME_BOARD = Board(BOARD_SIZE)
 CURRENT_COLOR = BLACK
 BLACK_POS, WHITE_POS = [], []
+STEP_COUNTER = 0
 
 
 # 计算窗口的大小
@@ -136,9 +137,9 @@ while running:
 
         # 处理其他事件，例如鼠标点击，添加棋子等
         if event.type == pygame.MOUSEBUTTONDOWN:
+
             # 左键下棋
             if event.button == 1:  # 1 代表鼠标左键
-                # print("last board", MEMORY[-1])
                 GAME_BOARD.translate(MEMORY[-1])
                 x_pos, y_pos = event.pos
                 x_board, y_board = position_translate(x_pos, y_pos)
@@ -162,25 +163,40 @@ while running:
                         continue
                     MEMORY.append(new_board[:])
                     CURRENT_COLOR = BLACK
-                # print("MEMORY:", MEMORY[-1])
 
+                # 当前步数加一
+                STEP_COUNTER += 1
+
+                # 计算目数
                 black_territory, white_territory, BLACK_POS, WHITE_POS = GAME_BOARD.count_territory()
 
-                print("Black:", black_territory, "\nWhite:", white_territory)
+                print("Step", STEP_COUNTER, "( Black:", black_territory, "\tWhite:", white_territory, ")")
 
             # 右键悔棋
             elif event.button == 3:  # 3 代表鼠标右键
                 if len(MEMORY) > 1:
+
+                    # 返回到上一个行动条
                     MEMORY.remove(MEMORY[-1])
+
+                    # 变更颜色
                     if CURRENT_COLOR == WHITE:
                         CURRENT_COLOR = BLACK
                     else:
                         CURRENT_COLOR = WHITE
-                # print("MEMORY:", MEMORY[-1])
+
+                # 当前步数减一
+                STEP_COUNTER -= 1
+
+                # 更新后端棋盘
                 GAME_BOARD.translate(MEMORY[-1])
+
+                # 计算目数
                 black_territory, white_territory, BLACK_POS, WHITE_POS = GAME_BOARD.count_territory()
+
     # 清空窗口
     window.fill((222, 184, 135))
+
     # 渲染棋盘
     draw_board()
 
